760a547406ffa6d98c62666dfc1b0c867b85adcd
deniseasc <denise1526alves@gmail.com>
2019-07-30 03:33:57 +0900

    javascript

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,25 @@
 // This is a JavaScript file
+$(document).on("click","#salvar",function(){
+  var parametros = {
+    "livro":$("#livro").val(),
+    "autor":$("#autor").val(),
+    "ano":$("#ano").val()
+  }
 
+  $.ajax({
+    type:"post", //como enviar
+    url:"http://livro3ds2.epizy.com/cadastra.php", //para onde enviar
+    //se der certo
+    success: function(data){
+      navigator.notification.alert(data);
+    $("#titulo").val(""),
+    $("#autor").val(""),
+    $("#ano").val("")
+    },
+    //se der errado
+    error: function(data){
+      navigator.notification.alert(data);
+    }
+  });
+
+});
